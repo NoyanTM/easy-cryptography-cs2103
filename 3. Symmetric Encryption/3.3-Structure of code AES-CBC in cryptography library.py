9e3e64f2ed265187173a945b75e3e68196af6b77
NoyanTM <67624929+NoyanTM@users.noreply.github.com>
2023-05-25 11:51:06 +0600

    Commit 3.3-Structure of code AES-CBC in cryptography library.py
    Commit 3.3-Structure of code AES-CBC in cryptography library.py

---
+++ 3. Symmetric Encryption/3.3-Structure of code AES-CBC in cryptography library.py
@@ -0,0 +1,25 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import os
+
+key = os.urandom(32)
+iv = os.urandom(16)
+
+aesCipher = Cipher(algorithms.AES(key),
+                   modes.CBC(iv),
+                   backend=default_backend())
+aesEncryptor = aesCipher.encryptor()
+aesDecryptor = aesCipher.decryptor()
+
+
+# Example of using / Testing
+plain = b"0123456789ABCDEF"
+cipher = aesEncryptor.update(plain)
+recover = aesDecryptor.update(cipher)
+if plain == recover:
+    print("[PASS]")
+else:
+    print("plain",plain)
+    print("cipher",cipher.hex())
+    print("recover",recover)
+    print("[FAIL]")
