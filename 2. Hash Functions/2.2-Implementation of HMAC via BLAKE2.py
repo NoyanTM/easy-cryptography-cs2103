e6034c8ee364acfd616cb412fcc8b01e7631d3ac
NoyanTM <67624929+NoyanTM@users.noreply.github.com>
2023-06-10 18:53:44 +0600

    Delete 2. Hash Functions directory

--- 2. Hash Functions/2.2-Implementation of HMAC via BLAKE2.py
+++
@@ -1,4 +0,0 @@
-from hashlib import blake2b
-h = blake2b(key=b'pseudorandom key', digest_size=16)
-h.update(b'message data')
-h.hexdigest()
--- 2. Hash Functions/2.3-Generating MD4 Windows hash.py
+++
@@ -1,5 +0,0 @@
-from Crypto.Hash import MD4 # pip install pycryptodome
-text = 'p@ssw0rd'
-hash_object = MD4.new(text.encode('utf-16le'))
-digest = hash_object.hexdigest()
-print(digest)
