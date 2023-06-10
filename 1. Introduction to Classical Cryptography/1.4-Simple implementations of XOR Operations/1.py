b3f5b26b73d5f919979fb1ce2e832b3a0145c19e
NoyanTM <67624929+NoyanTM@users.noreply.github.com>
2023-06-10 19:01:09 +0600

    Create 1.py

---
+++ 1. Introduction to Classical Cryptography/1.4-Simple implementations of XOR Operations/1.py
@@ -0,0 +1,10 @@
+x = int(input("Enter first decimal number: "))
+y = int(input("Enter second decimal number: "))
+
+res = x^y
+
+print("Decimal result:", res)
+
+print("Binary result:", format(res, "b"))
+print("Binary 1st number:", format(x, "b"))
+print("Binary 2nd number:", format(y, "b"))
