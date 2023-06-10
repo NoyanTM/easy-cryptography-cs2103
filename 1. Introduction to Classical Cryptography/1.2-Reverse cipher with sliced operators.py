64156c175070383ee86e05bc5a3805d84e939d08
NoyanTM <67624929+NoyanTM@users.noreply.github.com>
2023-06-10 18:53:33 +0600

    Delete 1. Introduction to Classical Cryptography directory

--- 1. Introduction to Classical Cryptography/1.2-Reverse cipher with sliced operators.py
+++
@@ -1,6 +0,0 @@
-def reverse_cipher(msg):
-        reverse_msg = msg[::-1]
-        return reverse_msg
-
-msg = input("Enter your message to reverse: ")
-print("Reversed message: ", reverse_cipher(msg))
--- 1. Introduction to Classical Cryptography/1.4-ASCII Encoding.py
+++
@@ -1,6 +0,0 @@
-message = "Hello, World!"
-
-# Convert string to ASCII representation
-ascii_representation = [ord(char) for char in message]
-
-print("ASCII Representation:", ascii_representation)
