2cadf3bc74b11277c8449df2c3f8ec7bc697e18d
NoyanTM <67624929+NoyanTM@users.noreply.github.com>
2023-06-10 18:54:42 +0600

    Delete 3. Symmetric Encryption directory

--- 3. Symmetric Encryption/3.1-DES via pyDES.py
+++
@@ -1,9 +0,0 @@
-import pyDes # Importing libraries (pyDES)
-
-
-data = “DES Algorithm” # Plaintext
-k = pyDes.des("DESCRYPT", pyDes.CBC, "\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5) # Initializing DES object
-d = k.encrypt(data) # Encrypting data using k object and stores it in d variable
-
-print("Encrypted: %r" % d)
-print("Decrypted: %r" % k.decrypt(d))
